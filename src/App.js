2f0bbbe511b4a8f8f718c314ba97794572b35200
gotonode <5214152+gotonode@users.noreply.github.com>
2018-10-16 19:26:16 +0300

    Building a Notes app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Note from './components/Note'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: props.notes
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Notes</h1>
+                <ul>
+                    {this.state.notes.map(note => <Note key={note.id} note={note} />)}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const notes = [
+    {
+        id: 1,
+        content: "First",
+        date: "2018",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Second",
+        date: "2018",
+        important: true
+    },
+    {
+        id: 3,
+        content: "Third",
+        date: "2018",
+        important: false
+    }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(< App notes={notes} />, document.getElementById('root'));
