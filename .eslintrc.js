65c9fadc85d2883af2df326edbad4c565f49cba5
gotonode <5214152+gotonode@users.noreply.github.com>
2018-10-28 00:47:39 +0300

    From spaces to tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "eqeqeq": "error",
-        "no-trailing-spaces": "error",
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "object-curly-spacing": [
-            "error", "always"
-        ],
-        "arrow-spacing": [
-            "error", { "before": true, "after": true }
-        ]
-    }
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 2018
+	},
+	"rules": {
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"object-curly-spacing": [
+			"error", "always"
+		],
+		"arrow-spacing": [
+			"error", { "before": true, "after": true }
+		]
+	}
 };
--- src/App.js
+++ src/App.js
@@ -5,116 +5,116 @@ import noteService from './services/notes'
 const errorDelay = 2000
 
 const Notification = ({ message }) => {
-    if (message === null || message.length === 0) {
-        return null
-    }
-    return (
-        <div id="error">
-            {message}
-        </div>
-    )
+	if (message === null || message.length === 0) {
+		return null
+	}
+	return (
+		<div id="error">
+			{message}
+		</div>
+	)
 }
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes: [],
-            showAll: true, // We show all notes, important and not important.
-            newNote: "",
-            errorMessage: null
-        }
-        console.log("Constructor called.")
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			notes: [],
+			showAll: true, // We show all notes, important and not important.
+			newNote: "",
+			errorMessage: null
+		}
+		console.log("Constructor called.")
+	}
 
-    eventHandler = (notes) => {
-        console.log("Promise fulfilled.")
-        this.setState({ notes })
-    }
+	eventHandler = (notes) => {
+		console.log("Promise fulfilled.")
+		this.setState({ notes })
+	}
 
-    componentDidMount() {
-        console.log("Mount.")
-        const promise = noteService.getAll()
-        promise.then(this.eventHandler)
-    }
+	componentDidMount() {
+		console.log("Mount.")
+		const promise = noteService.getAll()
+		promise.then(this.eventHandler)
+	}
 
-    addNote = (event) => {
-        // Prevent the page from reloading. This is a SPA, we don't need page reloads.
-        event.preventDefault()
+	addNote = (event) => {
+		// Prevent the page from reloading. This is a SPA, we don't need page reloads.
+		event.preventDefault()
 
-        if (this.state.newNote.trim().length === 0) {
-            alert("Please write something.")
-            return
-        }
+		if (this.state.newNote.trim().length === 0) {
+			// alert("Please write something.")
+			return
+		}
 
-        const noteObject = {
-            content: this.state.newNote,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5
-        }
+		const noteObject = {
+			content: this.state.newNote,
+			date: new Date().toISOString(),
+			important: Math.random() > 0.5
+		}
 
-        noteService.create(noteObject).then(newNote => {
-            this.setState({ notes: this.state.notes.concat(newNote), newNote: "" })
-        })
-    }
+		noteService.create(noteObject).then(newNote => {
+			this.setState({ notes: this.state.notes.concat(newNote), newNote: "" })
+		})
+	}
 
-    toggleVisible = () => {
-        this.setState({
-            showAll: !this.state.showAll
-        })
-    }
+	toggleVisible = () => {
+		this.setState({
+			showAll: !this.state.showAll
+		})
+	}
 
-    handleNoteChange = (event) => this.setState({ newNote: event.target.value })
+	handleNoteChange = (event) => this.setState({ newNote: event.target.value })
 
-    toggleImportanceOf = (id) => {
-        return () => {
+	toggleImportanceOf = (id) => {
+		return () => {
 
-            const note = this.state.notes.find(n => n.id === id)
-            const changedNote = { ...note, important: !note.important }
+			const note = this.state.notes.find(n => n.id === id)
+			const changedNote = { ...note, important: !note.important }
 
-            console.log(`Need to toggle importance of note ${note.id}.`)
+			console.log(`Need to toggle importance of note ${note.id}.`)
 
-            noteService.update(id, changedNote)
-                .then(changedNote => {
-                    const notes = this.state.notes.filter(n => n.id !== id)
-                    this.setState({ notes: notes.concat(changedNote) })
-                }).catch(error => {
-                    this.setState({
-                        notes: this.state.notes.filter(n => n.id !== id),
-                        errorMessage: `Note ${id} has already been deleted from the server.`
-                    })
-                    setTimeout(() => {
-                        this.setState({ errorMessage: null })
-                    }, errorDelay)
-                })
-        }
-    }
+			noteService.update(id, changedNote)
+				.then(changedNote => {
+					const notes = this.state.notes.filter(n => n.id !== id)
+					this.setState({ notes: notes.concat(changedNote) })
+				}).catch(error => {
+					this.setState({
+						notes: this.state.notes.filter(n => n.id !== id),
+						errorMessage: `Note ${id} has already been deleted from the server.`
+					})
+					setTimeout(() => {
+						this.setState({ errorMessage: null })
+					}, errorDelay)
+				})
+		}
+	}
 
 
-    render() {
+	render() {
 
-        const notesCollection = this.state.showAll ? this.state.notes : this.state.notes.filter(note => note.important === true)
+		const notesCollection = this.state.showAll ? this.state.notes : this.state.notes.filter(note => note.important === true)
 
-        const rows = () => notesCollection.map(note => <Note key={note.id} note={note} toggleImportance={this.toggleImportanceOf(note.id)} />)
+		const rows = () => notesCollection.map(note => <Note key={note.id} note={note} toggleImportance={this.toggleImportanceOf(note.id)} />)
 
-        const label = this.state.showAll === true ? "only important notes" : "all notes"
+		const label = this.state.showAll === true ? "only important notes" : "all notes"
 
-        return (
-            <div>
-                <h1>Notes</h1>
-                <Notification message={this.state.errorMessage} />
-                <ul>
-                    {rows()}
-                </ul>
-                <form onSubmit={this.addNote}>
-                    <input value={this.state.newNote} onChange={this.handleNoteChange} />
-                    <button type="submit" className="btn btn-primary">Save note</button>
-                    <button onClick={this.toggleVisible} className="btn btn-secondary">Show {label}</button>
-                </form>
-            </div>
-        )
-    }
+		return (
+			<div>
+				<h1>Notes</h1>
+				<Notification message={this.state.errorMessage} />
+				<ul>
+					{rows()}
+				</ul>
+				<form onSubmit={this.addNote}>
+					<input value={this.state.newNote} onChange={this.handleNoteChange} />
+					<button type="submit" className="btn btn-primary">Save note</button>
+					<button onClick={this.toggleVisible} className="btn btn-secondary">Show {label}</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default App
